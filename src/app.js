25bd69db8c65238ddc8ba7004a8fc1fea54c2869
Vansh <vanshchoudhary2744@gmail.com>
2025-07-17 23:37:39 +0530

    added cron in my app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import favouritesRoutes from "./routes/Favourites.route.js";
 
 export const app = express();
 
+app.get("/api/health", (req, res) => {
+  res.json({ success: true });
+});
+
 app.use(express.json());
 app.use(express.urlencoded());
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import { app } from "./app.js";
+import job from "./config/cron.js";
 import { ENV } from "./config/env.js";
+
+if (ENV.NODE_ENV === "production") job.start();
 
 const PORT = ENV.PORT;
 
